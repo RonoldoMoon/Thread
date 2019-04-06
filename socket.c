8269b10c74bab627bb3cf13ec4b0248666d557c6
RonoldoMoon <40582496+RonoldoMoon@users.noreply.github.com>
2019-04-06 18:47:16 +0000

    Add files via upload

---
+++ socket.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <netinet/in.h>
+#include <unistd.h>
+
+#define MAX 512
+//someday divorce range max from connection max :P
+//need struct with array pppointer iand index nummber(int).
+void Connection(void *p);
+
+int main()  {
+
+  struct sockaddr_in Address;
+  int                fd, x = 0;
+  int                SockArray  [MAX]; 
+  pthread_t          ThreadArray[MAX];
+  /*
+  if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)  {
+	   
+        printf("\n socket(): creation error \n"); 
+        exit(1);; 
+    } 
+   */
+   for ( ; x < MAX ; x++ )   {  // change for loop then wait on threads join.
+	   
+	 while(0); //(connection not yeald) { incrementAddress(); }
+	 
+	 //on success copy fd into array and spin off threaad.
+     pthread_create( &ThreadArray[x], NULL, Connection, NULL);
+    }
+} 
+
+void Connection(void *p)  {
+	
+	printf("connection details%d\n", 2); // p->count);
+	sleep(2);
+	//close(socket);
+}
